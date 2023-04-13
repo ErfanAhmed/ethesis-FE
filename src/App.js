a3b5baaf206c320f9a18a8c0cf51be90c520be75
Erfan Ahmed <erfan@therapservices.net>
2023-04-13 14:54:32 +0600

    Applied useState and useFetch pulled student info by id and showed in view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { Container, Divider, Typography } from '@mui/material';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
 
+  const [studentProfile, setStudentProfile] = useState([]); 
+
+  useEffect(() => {
+    fetch('http://localhost:8080/students/1')
+    .then(res => {
+      return res.json();
+    }).then(data => {
+      console.log(data);
+      setStudentProfile(data);
+    })
+  }, []);
+
   return (
     <div className="App">
-      <div className="content">
-        <h1>E-Thesis</h1>
-      </div>
+      <Container>
+        <Typography variant='h5'>Student Profile</Typography>
+        
+        <Divider/>
+
+        <Typography>Student ID: {studentProfile.studentId}</Typography>
+        <Typography>First Name: {studentProfile.firstName}</Typography>
+        <Typography>Last Name: {studentProfile.lastName}</Typography> 
+        <Typography>University: {studentProfile.university.name}</Typography> 
+        <Typography>Department: {studentProfile.department.code}</Typography> 
+        
+        </Container>
     </div>
   );
 }
